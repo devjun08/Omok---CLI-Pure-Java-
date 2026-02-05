da199a940be1d0345a50daf1686db54827ec2b91
KimJunYeop <devjun08@gmail.com>
2026-02-06 00:36:37 +0900

    feat: implement Player interface and LocalPlayer

--- src/player/LocalPlayer.java
+++ src/player/LocalPlayer.java
@@ -1,6 +1,35 @@
 package player;
 
+import board.Board;
+import java.util.Scanner;
+
 public class LocalPlayer implements Player {
-    public LocalPlayer(int i) {
+    private final int stoneType;
+    Scanner sc = new Scanner(System.in);
+
+    public LocalPlayer(int stoneType) {
+        this.stoneType = stoneType;
+    }
+
+    @Override
+    public int[] getNextMove(Board board) {
+        System.out.print("Where are you going to put it?(ex) A17): ");
+        String input = sc.next().toUpperCase();
+
+        while (true) {
+            try {
+                char col = input.charAt(0);
+                int row = Integer.parseInt(input.substring(1));
+
+                return new int[]{col, row};
+            } catch (Exception e) {
+                System.out.println("Invalid input format");
+            }
+        }
+    }
+
+    @Override
+    public int getStoneType() {
+        return stoneType;
     }
 }
