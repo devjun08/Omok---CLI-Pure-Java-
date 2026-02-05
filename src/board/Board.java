178e95f947d1ffb71bfb730ade8e3744bebf95c9
KimJunYeop <devjun08@gmail.com>
2026-02-05 23:30:14 +0900

    feat: implements stone placement logic

--- src/board/Board.java
+++ src/board/Board.java
@@ -4,14 +4,20 @@ public class Board {
     private static final int SIZE = 15;
     private final int[][] map = new int[SIZE][SIZE];
 
-    public void render() {
-        // 1. 상단 가로 번호 출력
-        for (int i = 0; i < SIZE; i++) {
-            System.out.printf("%2c ", (i + 'A'));
-        }
-        System.out.println();
+    // Board.java
+    public boolean place(int c, int r, int stoneType) {
+        c = c - 'A';
+        r = r - 1;
 
-        // 2. 바둑판 내용 및 세로 번호 출력
+        if (r < 0 || r >= SIZE || c < 0 || c >= SIZE || map[r][c] != 0) {
+            return false;
+        }
+        map[r][c] = stoneType;
+        return true;
+    }
+
+    public void render() {
+        // 1. 바둑판 내용 및 세로 번호 출력
         for (int r = 0; r < SIZE; r++) {
             for (int c = 0; c < SIZE; c++) {
                 // 현재 칸의 상태에 따른 심볼 결정
@@ -22,8 +28,15 @@ public class Board {
                 };
                 System.out.print(" " + symbol + " ");
             }
-            System.out.printf("%2d ", (15 - r)); // 좌측 세로 번호 (A~O)
+            System.out.printf("%2d ", (r + 1)); // 좌측 세로 번호 (A~O)
             System.out.println(); // 한 줄 완료 후 줄바꿈
         }
+
+        // 2. 하단 가로 문자 출력
+        for (int i = 0; i < SIZE; i++) {
+            System.out.printf("%2c ", (i + 'A'));
+        }
+
+        System.out.println();
     }
 }
