d83e347a678fff2e067cd6663fa3abd7cbc57c2b
KimJunYeop <devjun08@gmail.com>
2026-02-07 21:58:23 +0900

    Feat: implement core gamelogic with turn-based loop

--- src/engine/GameEngine.java
+++ src/engine/GameEngine.java
@@ -1,4 +1,49 @@
 package engine;
 
+import board.Board;
+import player.Player;
+
 public class GameEngine {
+    private final Board board;
+    private final Player blackPlayer;
+    private final Player whitePlayer;
+    private Player currentPlayer;
+
+    public GameEngine(Board board, Player p1, Player p2) {
+        this.board = board;
+        this.blackPlayer = p1;
+        this.whitePlayer = p2;
+        currentPlayer = blackPlayer;
+    }
+
+    public void run() {
+        System.out.println("Game Start!");
+        board.render();
+
+        while (true) {
+            String stoneColor = (currentPlayer == blackPlayer) ? "Black (●)" : "White (○)";
+            System.out.println("\nCurrent Player: " + stoneColor);
+
+            // Get coordinates from the current player
+            int move[] = currentPlayer.getNextMove(board);
+            int col = move[0];
+            int row = move[1];
+
+            // Try to place
+            boolean success = board.place(col, row, currentPlayer.getStoneType());
+
+            if (success) {
+                board.render();
+
+                switchPlayer(); // switch to the next player
+            } else {
+                System.out.println("Try again...");
+            }
+
+        }
+    }
+
+    public void switchPlayer() {
+        currentPlayer = (currentPlayer == blackPlayer) ? whitePlayer : blackPlayer;
+    }
 }
